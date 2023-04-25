f889aaa873408b1c30423959bf3eae1d8076bb72
Chazz Gagliano <cgags17285@gmail.com>
2023-04-25 14:12:07 -0400

    add signup action

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_25_175925) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_25_181051) do
   create_table "pets", force: :cascade do |t|
     t.string "name"
     t.string "breed"
@@ -18,6 +18,14 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_25_175925) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
